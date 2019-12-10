ff74f743707758c51c257dbb844d46c185e2fa00
Jiang Man jie <55577171+manjiej@users.noreply.github.com>
2019-12-10 22:35:40 +0800

    lib/job/summarize_job.rb created

---
+++ lib/jobs/summarize_job.rb
@@ -0,0 +1,39 @@
+require 'uri'
+require 'net/http'
+# require 'pry-byebug'
+
+class SummarizeJob < ApplicationJob
+  queue_as :default
+
+  def perform
+
+    summaries = Summary.where("title": "")
+
+    summaries.each do |summary|
+        @summary = summary
+        @data = data
+
+        summary.update(
+          "title": @data["objects"][0]["title"],
+          "text": @data["objects"][0]["text"]
+        )
+    end
+
+  end
+
+private
+
+  def data
+    JSON.parse(response)
+  end
+
+  def url
+    URI("https://api.diffbot.com/v3/article?token=606d0b61d6cf355ed6024deabc955e33&url=#{@summary.article_url}")
+  end
+
+  def response
+    Net::HTTP.get(url)
+  end
+
+
+end
