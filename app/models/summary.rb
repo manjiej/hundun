761d62f8ef0b5c94ba9df70b60625ec9fb785ce0
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2019-12-11 10:40:19 +0800

    Design bug fix

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -1,10 +1,8 @@
 class Summary < ApplicationRecord
   belongs_to :user, optional: true
-<<<<<<< HEAD
-=======
+
   belongs_to :taggable, polymorphic: true
 
->>>>>>> ae24e32f8824eb096ad5fba02139a83e148497c8
   validates :article_url, presence: true
 
   acts_as_taggable_on :tags
