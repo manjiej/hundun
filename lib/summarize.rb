b0d3c77b515d4a6a7b63261aa07688f0a6e17b77
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2019-12-12 13:13:00 +0800

    organized dashboard

--- lib/summarize.rb
+++ lib/summarize.rb
@@ -1,9 +1,12 @@
-class Summarize
+class Scrape
 
-  def self.digest url
+  def self.scrape url
     response = Net::HTTP.get(URI("https://api.diffbot.com/v3/article?token=606d0b61d6cf355ed6024deabc955e33&url=#{URI.encode(url)}"))
     parsed_res = JSON.parse(response)
     parsed_res["objects"][0]
+  end
+
+  def self.digest
   end
 
 end
