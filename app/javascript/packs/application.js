23c71011b7df961ec54bb9c397a46992eea3a70e
Yu Bai <white_by@126.com>
2019-12-16 19:36:23 +0800

    select2 add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,11 @@ import "bootstrap";
 
 console.log('Hello from application.js')
 
-import { previewImageOnFileSelect } from '../components/photo_preview';;
-previewImageOnFileSelect()
+import { previewImageOnFileSelect } from '../components/photo_preview';
+previewImageOnFileSelect();
+
+import 'select2/dist/css/select2.css';
+
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
